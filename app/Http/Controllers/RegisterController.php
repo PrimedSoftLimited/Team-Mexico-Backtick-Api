779edb39afeb0eb0500906958d6e0cbbe2d7e46e
jeremiahiro <jeremiahiro@gmail.com>
2019-03-12 09:01:15 +0200

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request){
+		$this->validateRequest($request);
+
+		$hasher = app()->make('hash');
+		$token = (str_random(60));
+
+		$user = User::create([
+			'username' => $request->input('username'),
+			'email' => $request->get('email'),
+			'phone_number' => $request->input('phone_number'),
+			'password'=> Hash::make($request->get('password')),
+			'api_token' => $token
+		]);
+		$res['message'] = "{$user->username} Created Successfully!";
+		$res['user'] = $user;
+		return response()->json($res, 202);
+    }
+
+    public function validateRequest(Request $request){
+		$rules = [
+            'username' => 'required|unique:users',
+						'email' => 'required|email|unique:users',
+            'phone' => 'required|numeric|phone' ,
+            'password' => 'required|min:6|confirmed'
+		];
+		$this->validate($request, $rules);
+
+    }
+}
